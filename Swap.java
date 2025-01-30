7b97ae2707472c14c864d6fceb897578b0e6749a
Kalyan31104 <vuppureddykalyan@gmail.com>
2025-01-30 21:17:47 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+class Swap{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+      //swapping without using third variable
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
