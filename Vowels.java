34d2924665beb274266da8fa9631329a60e5c800
Kalyan31104 <vuppureddykalyan@gmail.com>
2025-01-30 21:28:13 +0530

    Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -1,17 +1,29 @@
-
 import java.util.Scanner;
-
-class Vowels{
-    public static void main(String[] args) {
+public class Main
+{
+public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         char c=sc.next().charAt(0);
 //converting char to Lowecase for reducing checking of capital vowels
         char ch=Character.toLowerCase(c);
-//checking whether given char is a vowel
-        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-            System.out.println("It is a vowel");
-        }
-        else{
+//checking whether given char is a vowel using switch case.
+        switch(ch){
+            case 'a':
+                System.out.println("It is a vowel");
+                break;
+            case 'e':
+                System.out.println("It is a vowel");
+                break;
+            case 'i':
+                System.out.println("It is a vowel");
+                break;
+            case 'o':
+                System.out.println("It is a vowel");
+                break;
+            case 'u':
+                System.out.println("It is a vowel");
+                break;
+            default:
             System.out.println("It is not a vowel");
         }
 
